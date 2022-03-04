642bf15e47b85b924975d7644f4501696c46c7ad
Luis Guisado <lguisadom@gmail.com>
2022-03-04 13:10:35 -0500

    Start

---
+++ src/main/java/com/lagm/webflux/example/ExampleWebFlux/ExampleWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.lagm.webflux.example.ExampleWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagm/webflux/example/ExampleWebFlux/ExampleWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm.webflux.example.ExampleWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
